aa8261a35c481e7cf6f789cf5f772b4a0bb28ed3
j.e.liu <j.e.liu@accenture.com>
2022-06-19 14:12:23 +0300

    fix: Uncaught ReferenceError: y is not defined

--- craco.config.js
+++ craco.config.js
@@ -2,6 +2,11 @@ const CracoLessPlugin = require("craco-less");
 const CracoAlias = require("craco-alias");
 
 module.exports = {
+  babel: {
+    loaderOptions: {
+      ignore: ['./node_modules/mapbox-gl/dist/mapbox-gl.js'],
+    },
+  },
   plugins: [
     {
       plugin: CracoLessPlugin,
